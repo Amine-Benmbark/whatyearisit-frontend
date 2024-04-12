9d2ad474d16481499ff81a89ea4a639f52e68f24
amine_benmbark@hotmail.com <dortmund91000@hotmail.com>
2024-04-12 15:33:28 +0200

    kk

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 fetch('https://whatyearisit-backend-xi-nine.vercel.app/year')
     .then(response => response.json())
     .then(data => {
-        document.querySelector('#year').textContent = data.year.toString(); 
+        document.querySelector('#year').textContent = data.year; 
     })
     .catch(error => {
         console.error('Error fetching data:', error);
