45a12aee03ac46a11dd5a517e3f1a3a093d455f6
amine_benmbark@hotmail.com <dortmund91000@hotmail.com>
2024-04-12 15:16:48 +0200

    dde

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-fetch('http://localhost:3000/year')
+fetch('https://whatyearisit-backend-xi-nine.vercel.app/year')
     .then(response => response.json())
     .then(data => {
-        document.querySelector('#year').textContent = data.year; 
+        document.querySelector('#year').textContent = data.year.toString(); 
     })
     .catch(error => {
         console.error('Error fetching data:', error);
